e6527f9e44e220fe27794e1580928597855fce02
Pius Wong <pius@pioslabs.com>
2017-12-03 11:32:56 -0600

    Start clock placement with canvas

--- main.js
+++ main.js
@@ -3,13 +3,16 @@ window.onload = function(){
   // Visualize clock
   // Get canvas sizes
   var clockElem = document.getElementById('clockVis');
-  var clock = {};
-  clock.height = clockElem.clientHeight;
-  clock.width = clockElem.clientWidth;
   // Display clock graphics
   var ctx = clockElem.getContext('2d');
   var center = {};
-  center.x = Math.round(clock.width/2);
-  center.y = Math.round(clock.height/2);
-  
+  center.x = Math.round(clockElem.width/2);
+  center.y = Math.round(clockElem.height/2);
+  console.log(center);
+  // Show minute clock
+  ctx.beginPath();
+  ctx.arc(100, 75, 50, 0, 1.5 * Math.PI);
+  ctx.lineWidth=10;
+  ctx.strokeStyle="#FF0000";
+  ctx.stroke();
 };
