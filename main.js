efb9a60fee2408712af07fce5a733b914d1fa92d
Pius Wong <pius@pioslabs.com>
2017-12-03 16:49:01 -0600

    Clock shows default minute and second hand

--- main.js
+++ main.js
@@ -29,16 +29,21 @@ window.onload = function(){
     var center = {};
     center.x = Math.round(clockElem.width/2);
     center.y = Math.round(clockElem.height/2);
-    console.log(center);
+    var radiusDefault = 0.4*clockElem.width;
+    var thicknessDefault = (0.05)*clockElem.width;
     // Show minute clock
     ctx.beginPath();
-    ctx.arc(center.x, center.x, 100, 0.5*Math.PI, 1.0*Math.PI); // center, rad, start,end
-    ctx.lineWidth=10; // thickness
+    ctx.arc(center.x, center.y, radiusDefault, 0.3*2*Math.PI, 0.45*2*Math.PI); // center, rad, start,end
+    ctx.lineWidth=thicknessDefault;
     ctx.strokeStyle='#79BD9A'; // minutes?
     ctx.stroke();
-
-
-    '0B486B' // hours?
+    // Show second clock
+    ctx.beginPath();
+    ctx.arc(center.x, center.y, radiusDefault, 0.87*2*Math.PI, 0.93*2*Math.PI); // center, rad, start,end
+    ctx.lineWidth=thicknessDefault;
+    ctx.strokeStyle='#CFF09E'; // seconds?
+    ctx.stroke();
+//    '0B486B' // shadow? dark blue
   }
 
 };
