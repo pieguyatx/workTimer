8ba45a0692c420ac6a2cc183912e90718ece4490
Pius Wong <pius@pioslabs.com>
2017-12-03 16:23:06 -0600

    Clock canvas dynamically resizes & redraws upon window resize

--- main.js
+++ main.js
@@ -3,19 +3,42 @@ window.onload = function(){
   // Visualize clock
   // Get canvas sizes
   var clockElem = document.getElementById('clockVis');
-  // Display clock graphics
-  var ctx = clockElem.getContext('2d');
-  var center = {};
-  center.x = Math.round(clockElem.width/2);
-  center.y = Math.round(clockElem.height/2);
-  console.log(center);
-  // Show minute clock
-  ctx.beginPath();
-  ctx.arc(center.x, center.x, 100, 0.5*Math.PI, 1.0*Math.PI); // center, rad, start,end
-  ctx.lineWidth=10; // thickness
-  ctx.strokeStyle='#79BD9A';
-  ctx.stroke();
+  var clockContainer = document.getElementById('clockContainer');
+
+  initializeCanvas();
+
+  function initializeCanvas(){
+    window.addEventListener('resize',resizeCanvas,false);
+    resizeCanvas();
+  }
+
+  function resizeCanvas(){
+    // position canvas
+    clockElem.width = clockContainer.clientWidth;
+    clockElem.height = clockContainer.clientWidth;
+    var squareLength = clockElem.width.toString() + "px";
+    clockContainer.style.height = squareLength;
+    clockElem.style.width = squareLength;
+    clockElem.style.height = squareLength;
+    redrawCanvas();
+  }
+
+  function redrawCanvas(){
+    // Display clock graphics
+    var ctx = clockElem.getContext('2d');
+    var center = {};
+    center.x = Math.round(clockElem.width/2);
+    center.y = Math.round(clockElem.height/2);
+    console.log(center);
+    // Show minute clock
+    ctx.beginPath();
+    ctx.arc(center.x, center.x, 100, 0.5*Math.PI, 1.0*Math.PI); // center, rad, start,end
+    ctx.lineWidth=10; // thickness
+    ctx.strokeStyle='#79BD9A'; // minutes?
+    ctx.stroke();
 
 
-  '0B486B'
+    '0B486B' // hours?
+  }
+
 };
